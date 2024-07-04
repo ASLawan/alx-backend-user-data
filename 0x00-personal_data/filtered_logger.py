0a550c67fbbec2c8371bc6a46004de351b7fe159
ASLawan <nawalnitsua@gmail.com>
2024-07-04 07:40:18 +0100

    Fix code output errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Uses regex to obfuscate and log data"""
-    pattern = f"({'|'.join(fields)})=[^separator]*"
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
     # replacement = lambda m: f"{m.group(1)}={redaction}"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
