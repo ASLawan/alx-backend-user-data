c4a3dbf2ea7f4171e546ee6eaa90dd6fc4ed142d
ASLawan <nawalnitsua@gmail.com>
2024-07-04 08:28:29 +0100

    Implement format class method to filter values in incoming log records using filter_datum

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
     Module implementing logging with obfuscation
 
 """
+import logging
 import re
 from typing import List
 
@@ -13,3 +14,20 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     # replacement = lambda m: f"{m.group(1)}={redaction}"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes FORMAT class attribute"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values for incoming log records"""
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
