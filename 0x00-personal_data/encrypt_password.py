afcf132afedf10b7c02a71192553411e9ace716d
ASLawan <nawalnitsua@gmail.com>
2024-07-08 04:06:31 +0100

    Implement password hashing using bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Module implementing a password-hashing function
+
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt and returns a salted hashed password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
