9f6278b7112bf597ff7fedf5fc59825492ff4960
ASLawan <nawalnitsua@gmail.com>
2024-07-08 15:41:26 +0100

    Implement a method in BasicAuth class that retrieves a User instance by credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -53,3 +55,21 @@ class BasicAuth(Auth):
         credentials = decoded_base64_authorization_header.split(":", 1)
         email, passwd = credentials
         return email, passwd
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns User isntance based on email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
