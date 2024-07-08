e2dfb435b40756a47bbf82c93ccab9f5c1834166
ASLawan <nawalnitsua@gmail.com>
2024-07-08 13:54:38 +0100

    Create class BasicAuth that inherits from Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Module implementing BasicAuth class
+
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that extends Auth class"""
+    pass
