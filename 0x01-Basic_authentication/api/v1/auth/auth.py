2c092470d17d40eed6ce20455713f1758c94533d
ASLawan <nawalnitsua@gmail.com>
2024-07-08 12:08:17 +0100

    Update require_auth class method to handle check of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,31 @@
 #!/urs/bin/env python3
-""" 
+"""
     Module implementing Auth class
 
 """
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """Manages the API authentiction"""
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Requires authentication"""
-        return False
-
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path += "/"
+        for excluded_path in excluded_paths:
+            if excluded_path == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authentiction header"""
         return None
 
-    
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns current user"""
         return None
