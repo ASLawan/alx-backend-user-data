436aa247b5e836aff8da01c6a1fcd75ba5051b7d
ASLawan <nawalnitsua@gmail.com>
2024-07-11 09:50:40 +0100

    Define and empty SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Module implementing SessionAuth class
+
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session class that inherits from Auth class"""
+    pass
