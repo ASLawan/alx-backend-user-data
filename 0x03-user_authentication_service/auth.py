4e4724d8c956000cdeb95360b0353e82b5d16030
ASLawan <nawalnitsua@gmail.com>
2024-07-16 15:06:22 +0100

    Implement destroy_session method that ends current user session based on user id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,11 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys current session for a given user based on user id"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id. session_id=None)
+        except NoResultFound:
+            pass
