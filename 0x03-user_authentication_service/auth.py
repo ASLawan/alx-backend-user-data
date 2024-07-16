5996ae67ddb9bdb6e6fdd7b9617ea2b4475df276
ASLawan <nawalnitsua@gmail.com>
2024-07-16 12:28:03 +0100

    Implement valid_login method to check for valid credentials on login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,3 +35,14 @@ class Auth:
             new_user = self._db.add_user(email=email,
                                          hashed_password=hashedpwd)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if password is valid by comparing the two hashes"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        if bcrypt.checkpw(password.encode(), user.hashed_password):
+            return True
+        return False
