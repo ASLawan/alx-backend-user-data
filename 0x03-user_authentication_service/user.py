2bfeacc76061e134053647f22e745729db76fb25
ASLawan <nawalnitsua@gmail.com>
2024-07-16 01:46:20 +0100

    Fix output errors

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,9 +13,9 @@ Base = declarative_base()
 class User(Base):
     """class User that inherits from the Base class"""
 
-    __table__ = "users"
+    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, auto_increment=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False, unique=True)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
