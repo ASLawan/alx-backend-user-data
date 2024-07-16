983fe435efa97cafe758077ffe0b8c46d2c0a85f
ASLawan <nawalnitsua@gmail.com>
2024-07-16 02:30:53 +0100

    Implement the find_user_by method in DB class that finds a user based on given kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 from user import Base, User
 
 
@@ -34,3 +37,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based given kwargs"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found for given kwargs")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid  query parameters")
